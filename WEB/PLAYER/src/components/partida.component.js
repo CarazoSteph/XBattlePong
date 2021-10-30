c26762dab912194517cab59dcd7b7261eff71e2f
Yenson Umaña Solano <yen21000@gmail.com>
2021-10-30 15:08:13 -0600

    Place ship basic feature added

--- WEB/PLAYER/src/components/partida.component.js
+++ WEB/PLAYER/src/components/partida.component.js
@@ -7,18 +7,37 @@ export default class Partida extends Component {
     this.state = {
       currentEvent: '',
       valueEventName: '',
-      filas: '',
-      columnas: ''
-
+      filas: 10,
+      columnas: 10,
+      gameArr:''
     }
+    this.seaIMG = 'https://github.com/CarazoSteph/XBattlePong/blob/develop/WEB/PLAYER/src/resources/sea-icon-9.png?raw=true'
+    this.ship1IMG = 'https://github.com/CarazoSteph/XBattlePong/blob/develop/WEB/PLAYER/src/resources/icon-cruise-ship.png?raw=true'
     this.handleChangeEventName = this.handleChangeEventName.bind(this);
     this.handleSubmitNewEvent = this.handleSubmitNewEvent.bind(this);
+    this.shipNow=''
   }
 
   handleChangeEventName(event) {
     this.setState({ valueEventName: event.target.value });
   }
   handleSubmitNewEvent(event) {
+  }
+
+  componentDidMount(){
+    this.setState({ gameArr: Array.from({length: this.state.filas}, (v, a) => Array.from({length: this.state.columnas}, (v, i) => [a,i,this.seaIMG,false]))});
+  }
+
+  estaArr(list){
+    return list.includes(this.shipNow)
+  }
+  onPlace(ship){
+    // eslint-disable-next-line react/no-direct-mutation-state
+    this.state.gameArr[ship[0]][ship[1]][2]=this.ship1IMG
+    // eslint-disable-next-line react/no-direct-mutation-state
+    this.state.gameArr[ship[0]][ship[1]][3]=true
+    console.log(this.state.gameArr[ship[0]][ship[1]])
+    this.forceUpdate()
   }
 
   render() {
@@ -38,99 +57,52 @@ export default class Partida extends Component {
 
             <table class="table table-bordered mx-3">
               <thead>
-                <tr>
-                  <th scope="col"></th>
-                  <th scope="col">A</th>
-                  <th scope="col">B</th>
-                  <th scope="col">C</th>
-                  <th scope="col">D</th>
-                  <th scope="col">E</th>
-
+              <tr>
+              <th scope="col"></th>
+              {Array.from({length:this.state.columnas}).map((row,num) =>
+                  <th scope="col">{num+1}</th>
+                )}
                 </tr>
+                
               </thead>
               <tbody>
-                <tr>
-                  <th scope="row">1</th>
-                  <td >
-                    <button type="button" class="btn btn-outline-primary">
-                      <img src="https://www.cdc.gov/legionella/images/icon-cruise-ship.png" alt=' ' class="card-img-top img-fluid" style={{ 'max-width': '50px' }} />
-                    </button>
-                  </td>
-                  <td >
-                    <button type="button" class="btn btn-outline-primary">
-                      <img src="https://www.cdc.gov/legionella/images/icon-cruise-ship.png" alt=' ' class="card-img-top img-fluid" style={{ 'max-width': '50px' }} />
-                    </button>
-                  </td>
-                  <td >
-                    <button type="button" class="btn btn-outline-primary">
-                      <img src="https://www.cdc.gov/legionella/images/icon-cruise-ship.png" alt=' ' class="card-img-top img-fluid" style={{ 'max-width': '50px' }} />
-                    </button>
-                  </td>
-                  <td ><button type="button" class="btn btn-outline-primary">
-                    <img src="https://www.cdc.gov/legionella/images/icon-cruise-ship.png" alt=' ' class="card-img-top img-fluid" style={{ 'max-width': '50px' }} />
-                  </button></td>
-                  <td >
-                    <button type="button" class="btn btn-outline-primary">
-                      <img src="https://www.cdc.gov/legionella/images/icon-cruise-ship.png" alt=' ' class="card-img-top img-fluid" style={{ 'max-width': '50px' }} />
-                    </button>
-                  </td>
-                </tr>
-                <tr>
-                  <th scope="row">2</th>
-                  <td >An ship</td>
-                  <td >A second ship</td>
-                  <td >A third ship</td>
-                  <td >An ship</td>
-                  <td >A second ship</td>
-                </tr>
-                <tr>
-                  <th scope="row">3</th>
-                  <td >An ship</td>
-                  <td >A second ship</td>
-                  <td >A third ship</td>
-                  <td >An ship</td>
-                  <td >A second ship</td>
-                </tr>
-                <tr>
-                  <th scope="row">4</th>
-                  <td >An ship</td>
-                  <td >A second ship</td>
-                  <td >A third ship</td>
-                  <td >An ship</td>
-                  <td >A second ship</td>
-                </tr>
-                <tr>
-                  <th scope="row">5</th>
-                  <td >An ship</td>
-                  <td >A second ship</td>
-                  <td >A third ship</td>
-                  <td >An ship</td>
-                  <td >A second ship</td>
-                </tr>
+                
+                {Array.from(this.state.gameArr).map((row,num) =>
+                  <tr>
+                  <th scope="row">{num+1}</th>
+                  {Array.from(row).map((ship) =>
+                      <td>
+                        <button type="button" class="btn btn-outline-primary" onClick={()=>this.onPlace(ship)}>
+                          <img src={ship[2]} alt=' ' class="card-img-top img-fluid" style={{ 'maxWidth': '50px' }} />
+                        </button>
+                      </td>
+                      )}</tr>)}
               </tbody>
             </table>
           </div>
           <div class="col-sm-2">
             <div class="card border-secondary mb-3 mx-3">
-              <div class="card-header">Nave 1, 2x2</div>
+              <div class="card-header">Nave 1, 1x1</div>
               <div class="card-body text-secondary">
                 <button type="button" class="btn btn-outline-primary">
-                  <img src="https://www.cdc.gov/legionella/images/icon-cruise-ship.png" alt=' ' class="card-img-top img-fluid" style={{ 'max-width': '50%' }} />
+                  <img src={this.ship1IMG} alt=' ' class="card-img-top img-fluid" style={{ 'max-width': '50%' }} />
                 </button>
               </div>
             </div>
             <div class="card border-success mb-3 mx-3">
               <div class="card-header">Nave 2</div>
               <div class="card-body text-success">
-                <img src="https://www.cdc.gov/legionella/images/icon-cruise-ship.png" alt=' ' class="card-img-top img-fluid" style={{ 'max-width': '50%' }} />
-                <p class="card-text"></p>
+                <button type="button" class="btn btn-outline-primary">
+                  <img src={this.ship1IMG} alt=' ' class="card-img-top img-fluid" style={{ 'max-width': '50%' }} />
+                </button>
               </div>
             </div>
             <div class="card border-danger mb-3 mx-3">
               <div class="card-header">Nave 3</div>
               <div class="card-body text-danger">
-                <img src="https://www.cdc.gov/legionella/images/icon-cruise-ship.png" alt=' ' class="card-img-top img-fluid" style={{ 'max-width': '50%' }} />
-                <p class="card-text"></p>
+                <button type="button" class="btn btn-outline-primary">
+                  <img src={this.ship1IMG} alt=' ' class="card-img-top img-fluid" style={{ 'max-width': '50%' }} />
+                </button>
               </div>
             </div>
           </div>
